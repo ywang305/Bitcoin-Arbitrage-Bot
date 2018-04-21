862d13bc646780927f78e5d6a0f0a975aed2ddd3
ywang305 <ywang305@syr.edu>
2018-04-21 01:34:15 -0400

    navigator, fixed grid as stateless component, and put fetch data method into App's navbar

--- Mern/src/App.jsx
+++ Mern/src/App.jsx
@@ -2,11 +2,96 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ChartGrid from './ChartGrid.jsx';
+import { Glyphicon, Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
+class AppStart extends React.Component {
+	constructor(props) {
+		super(props);
 
+		this.play = this.play.bind(this);
+		this.pause = this.pause.bind(this);
+		this.state = { Ticks:[],  };
+	}
+	play() {
+		if(this.timerID) return;
+		// use lambda to bind "this"
+		let poll = ()=>{
+			this.loadData('/api/btc-tick');
+			this.loadData('/api/btc-trade');
+			this.timerID = setTimeout(poll, 1000);
+		};
+		this.timerID = setTimeout( poll , 1000 );
+	}
+
+	pause() {
+		if(this.timerID) {
+			clearTimeout(this.timerID);
+			this.timerID = undefined;
+		}
+	}
+	
+
+	loadData(uri) {
+		fetch(uri)
+			.then( res => {
+				if(res.ok) {
+					res.json().then( data => {
+						console.log(data);
+						this.update(data);
+					});
+				}
+			}).catch( err=>{
+				console.log(`Error in feching data from server: ${err}`);
+			});
+	}
+
+	update(data) {
+		if(data.type && data.type=='tick') {
+			this.setState({Ticks: data.ticks});
+		}
+	}
+	
+	
+
+	render() {
+		return (
+			<div> 
+				<Navbar inverse collapseOnSelect>
+					<Navbar.Header>
+						<Navbar.Brand><a href='#'>Bitcoin-Arbitrage Charts</a></Navbar.Brand>
+						<Navbar.Toggle />
+					</Navbar.Header>
+					<Navbar.Collapse>
+						<Nav>
+							<NavItem>
+								<Button bsStyle="primary" onClick={this.play}>
+									<Glyphicon glyph="play" />
+								</Button> 
+							</NavItem>
+							<NavItem>
+								<Button bsStyle="success" onClick={this.pause}>
+									<Glyphicon glyph="pause" />
+								</Button>
+							</NavItem>
+						</Nav>
+						<Nav pullRight>
+							<NavDropdown id="user-dropdown" title="Exchanges" noCaret>
+								<MenuItem>Coinbase</MenuItem>
+								<MenuItem>Bitfinex</MenuItem>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+
+				<ChartGrid ticks={this.state.Ticks} />
+			
+			</div>
+		);
+	}
+}
 
 const contentNode = document.getElementById('contents');
-ReactDOM.render(<ChartGrid />, contentNode);
+ReactDOM.render(<AppStart />, contentNode);
 
 
 
--- Mern/src/ChartGrid.jsx
+++ Mern/src/ChartGrid.jsx
@@ -1,95 +1,25 @@
 import React from 'react';
 import PriceChart from './Chart.jsx';
-import { Grid, Row, Col, Button,  ButtonToolbar } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 
   
+const ChartGrid = (props) => (
+	<Grid>
+		<Row> 
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Coinbase' ticks={props.ticks}/> </Col>
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Gemini' ticks={props.ticks}/> </Col> 
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Itbit' ticks={props.ticks}/> </Col>    
+
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Btcc' ticks={props.ticks}/> </Col>
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Bitstamp' ticks={props.ticks}/> </Col> 
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Bitfinex' ticks={props.ticks}/> </Col>
+
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Cex' ticks={props.ticks}/> </Col>
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Bitbay' ticks={props.ticks}/> </Col> 
+			<Col xs={12} sm={6} md={4}> <PriceChart title='Gdax' ticks={props.ticks}/> </Col>   
+		</Row>
+	</Grid>
+);
 
 
-export default class CharGrid extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { Ticks:[],  };
-		this.play = this.play.bind(this);
-		this.pause = this.pause.bind(this);
-	}
-
-	/*
-	componentDidMount() {
-		this.timerID = setInterval(
-			() => {
-				this.loadData('/api/btc-tick');
-				this.loadData('/api/btc-trade');
-			}
-			,1000
-		);
-	}
-	componentWillUnmount() {
-		clearInterval(this.timerID);
-	}
-	*/
-
-	play() {
-		if(this.timerID) return;
-		// use lambda to bind "this"
-		let poll = ()=>{
-			this.loadData('/api/btc-tick');
-			this.loadData('/api/btc-trade');
-			this.timerID = setTimeout(poll, 1000);
-		};
-		this.timerID = setTimeout( poll , 1000 );
-	}
-
-	pause() {
-		if(this.timerID) {
-			clearTimeout(this.timerID);
-			this.timerID = undefined;
-		}
-	}
-
-	loadData(uri) {
-		fetch(uri)
-			.then( res => {
-				if(res.ok) {
-					res.json().then( data => {
-						console.log(data);
-						this.update(data);
-					});
-				}
-			}).catch( err=>{
-				console.log(`Error in feching data from server: ${err}`);
-			});
-	}
-
-	update(data) {
-		if(data.type && data.type=='tick') {
-			this.setState({Ticks: data.ticks});
-		}
-	}
-	
-
-	render() {
-		return (
-			<Grid>
-				<Row>
-					<ButtonToolbar>
-						<Button bsSize='small' bsStyle="primary" onClick={this.play}>start</Button>
-						<Button bsSize='small' bsStyle="warning" onClick={this.pause}>pause</Button>
-					</ButtonToolbar>
-				</Row>
-				<Row> 
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Coinbase' ticks={this.state.Ticks}/> </Col>
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Gemini' ticks={this.state.Ticks}/> </Col> 
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Itbit' ticks={this.state.Ticks}/> </Col>    
-
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Btcc' ticks={this.state.Ticks}/> </Col>
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Bitstamp' ticks={this.state.Ticks}/> </Col> 
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Bitfinex' ticks={this.state.Ticks}/> </Col>
-    
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Cex' ticks={this.state.Ticks}/> </Col>
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Bitbay' ticks={this.state.Ticks}/> </Col> 
-					<Col xs={12} sm={6} md={4}> <PriceChart title='Gdax' ticks={this.state.Ticks}/> </Col>   
-				</Row>
-			</Grid>	
-		);
-	}
-}
+export default ChartGrid;
